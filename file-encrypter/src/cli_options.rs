f2240a5ec31678c0aedb0730b85cff0fbc07fe4e
Evan Schoening <evan.schoening@gmail.com>
2024-04-30 16:16:15 -0700

    added arg parser for this

--- file-encrypter/src/cli_options.rs
+++ file-encrypter/src/cli_options.rs
@@ -2,3 +2,21 @@
 
 // option for phrase, file, or dir
 // also need output name if optional
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    // File to encrypt or decrypt, eventually make this an array
+    #[arg(short, long)]
+    file_paths: Vec[String],
+
+    // Password to encrypt file with
+    #[arg(short, long)]
+    password: String,
+
+    //decrypt option 
+    #[arg(short, long, default_value_t = False)]
+    decrypt: bool
+}
--- file-encrypter/src/main.rs
+++ file-encrypter/src/main.rs
@@ -1,10 +1,16 @@
 use simple_crypt::{encrypt, encrypt_file};
 use std::path::Path;
+use clap::Parser;
+
+mod cli_options::Args;
 
 
 
 fn main() {
     println!("Hello, world!");
+    let file_args = Args::parse()
     // first encrypt a file and see the outpt
+    // Need to verify return type
+    // Need to get the args added in here for correct usage 
     let _ = encrypt_file(Path::new("./test.txt"), Path::new("./encrypted-text.txt"), b"test");
 }
