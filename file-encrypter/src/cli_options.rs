f4a59a073677abd962c812298eefa106bf2a9f48
Evan Schoening <evan.schoening@gmail.com>
2024-05-08 13:45:49 -0700

    working cli tools to encrypt files

--- file-encrypter/src/cli_options.rs
+++ file-encrypter/src/cli_options.rs
@@ -1,22 +1,22 @@
-
-
-// option for phrase, file, or dir
-// also need output name if optional
 use clap::Parser;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-pub struct Args {
+pub struct FileArgs {
     // File to encrypt or decrypt, eventually make this an array
     #[arg(short, long)]
-    file_paths: Vec[String],
+    pub file_paths: Vec<String>,
 
     // Password to encrypt file with
     #[arg(short, long)]
-    password: String,
+    pub password: String,
 
     //decrypt option 
-    #[arg(short, long, default_value_t = False)]
-    decrypt: bool
+    #[arg(short, long, default_value_t = false)]
+    pub decrypt: bool,
+
+    //delete original file; this would simulate a malware, where files become encrypted 
+    #[arg(long, default_value_t = false)]
+    pub delete_original: bool
 }
--- file-encrypter/src/main.rs
+++ file-encrypter/src/main.rs
@@ -1,16 +1,22 @@
 use simple_crypt::{encrypt, encrypt_file};
 use std::path::Path;
 use clap::Parser;
+use std::fmt::format;
 
-mod cli_options::Args;
+mod cli_options;
 
 
 
 fn main() {
     println!("Hello, world!");
-    let file_args = Args::parse()
+    let file_args: cli_options::FileArgs = cli_options::FileArgs::parse();
     // first encrypt a file and see the outpt
+    // todo need a check to ensure vec is not null
+    let paths = file_args.file_paths;
+    for path in paths {
+        let encyrpted_file = encrypt_file(Path::new(&path), Path::new(&format!("{}.enc", path)), b"test");
+    }
     // Need to verify return type
     // Need to get the args added in here for correct usage 
-    let _ = encrypt_file(Path::new("./test.txt"), Path::new("./encrypted-text.txt"), b"test");
+    //let _ = encrypt_file(Path::new("./test.txt"), Path::new("./encrypted-text.txt"), b"test");
 }
