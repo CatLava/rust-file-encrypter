14ca6b34f378dc429bb01e9b9992c2ada552e176
Evan Schoening <evan.schoening@gmail.com>
2024-05-13 13:37:01 -0700

    working decrypter

--- file-encrypter/src/main.rs
+++ file-encrypter/src/main.rs
@@ -1,7 +1,9 @@
-use simple_crypt::{encrypt, encrypt_file};
+use simple_crypt::{encrypt, encrypt_file, decrypt, decrypt_file};
 use std::path::Path;
+use std::fs;
 use clap::Parser;
 use std::fmt::format;
+
 
 mod cli_options;
 
@@ -13,8 +15,20 @@ fn main() {
     // first encrypt a file and see the outpt
     // todo need a check to ensure vec is not null
     let paths = file_args.file_paths;
-    for path in paths {
-        let encyrpted_file = encrypt_file(Path::new(&path), Path::new(&format!("{}.enc", path)), b"test");
+    let password = file_args.password;
+    if file_args.decrypt{
+        println!("Attempting to decrypt file");
+        for path in paths {
+            let decyrpted_file = decrypt_file(Path::new(&path), Path::new(&format!("{}.fixed", path)), password.as_bytes());
+        }
+    } else {
+        for path in paths {
+            let encyrpted_file = encrypt_file(Path::new(&path), Path::new(&format!("{}.enc", path)), password.as_bytes());
+            if file_args.delete_original {
+                fs::remove_file(&path).expect("unable to remove file")
+    
+            }
+        }
     }
     // Need to verify return type
     // Need to get the args added in here for correct usage 
